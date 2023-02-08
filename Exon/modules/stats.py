d0ea124d13f9a1e69503dd909db0e03a5b2b0548
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-02-09 02:51:06 +0700

    Update stats.py

--- Exon/modules/stats.py
+++ Exon/modules/stats.py
@@ -51,8 +51,8 @@ async def bot_sys_stats():
 ⛖ ᴄᴘᴜ ᴜsᴀɢᴇ : {cpu}%
 ⛖ ʀᴀᴍ ᴜsᴀɢᴇ : {mem}%
 ⛖ ᴅɪsᴋ ᴜsᴀɢᴇ : {disk}%
-⛖ ᴜsᴇʀs : 0{users_db.num_users()} ᴜsᴇʀs.
-⛖ ɢʀᴏᴜᴘs : 0{users_db.num_chats()} ɢʀᴏᴜᴘs.
+⛖ ᴜsᴇʀs : {sql.num_users()} ᴜsᴇʀs.
+⛖ ɢʀᴏᴜᴘs : {sql.num_chats()} ɢʀᴏᴜᴘs.
 """
 
     return stats
